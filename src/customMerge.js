e9487a0829d2f1d3212cf08ded56e761c9e6fffc
Chris <slicejunk@gmail.com>
2018-04-09 07:50:34 +0200

    performance improvement
    https://github.com/GeekyAnts/NativeBase/issues/1536
    there are some iteration on the properties of both objects, I guess this can be be optimized by removing the inner iteration

--- src/customMerge.js
+++ src/customMerge.js
@@ -1,6 +1,5 @@
 export default function customMerge(obj1, obj2) {
   const objToReturn = {};
-
   let property1, property2;
 
   if (!obj1) {
@@ -10,29 +9,27 @@ export default function customMerge(obj1, obj2) {
   }
 
   for (property1 in obj1) {
-    for (property2 in obj2) {
-      if (property1 === property2) {
-        if (
-          typeof obj1[property1] !== "object" ||
-          typeof obj2[property1] !== "object" ||
-          !obj2[property1] ||
-          !obj1[property1]
-        ) {
-          objToReturn[property1] = obj2[property1];
-        } else {
-          objToReturn[property1] = customMerge(
-            obj1[property1],
-            obj2[property1]
-          );
-        }
+    if (obj2[property1] != null) {
+      if (
+        typeof obj1[property1] !== 'object' ||
+        typeof obj2[property1] !== 'object' ||
+        !obj2[property1] ||
+        !obj1[property1]
+      ) {
+        objToReturn[property1] = obj2[property1];
       } else {
-        if (objToReturn[property1] === undefined)
-          objToReturn[property1] = obj1[property1];
-        if (objToReturn[property2] === undefined)
-          objToReturn[property2] = obj2[property2];
+        objToReturn[property1] = customMerge(obj1[property1], obj2[property1]);
       }
+    } else {
+      objToReturn[property1] = obj1[property1];
     }
   }
-
+  for (property2 in obj2) {
+    if (obj1[property2] != null) {
+      // already merged !
+    } else {
+      objToReturn[property2] = obj2[property2];
+    }
+  }
   return objToReturn;
 }
